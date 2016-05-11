48e7ccdffd938a509d8ca946f5f0f9e0ff20918c
Natthakit Susanthitanon <natthakit.susanthitanon@gmail.com>
2016-05-11 19:15:35 +0700

    Add buildMultipleMessages
    Also fix typo and change filenames

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ module.exports = function(config) {
   return bot.init(config);
 };
 
-module.exports.Markup = require('./lib/message/markup.js');
+module.exports.Markup = require('./lib/message/Markup.js');
+module.exports.MultipleMessages = require('./lib/message/MultipleMessages.js');
