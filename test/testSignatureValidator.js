e398f382380b68d3b64e3100efa72632f0e911d5
Panjamapong Sermsawatsri <panjmp@gmail.com>
2016-05-12 15:27:49 +0700

    fix tests

--- test/testSignatureValidator.js
+++ test/testSignatureValidator.js
@@ -10,52 +10,50 @@ describe('SignatureValidator', function() {
     channelSecret: 'testsecret',
     channelMID: 'u0a556cffd4da0dd89c94fb36e36e1cdc'
   };
-
-  var json = {
-    result: [
-      {
-        from: 'u206d25c2ea6bd87c17655609a1c37cb8',
-        fromChannel: '1341301815',
-        to: ['u0cc15697597f61dd8b01cea8b027050e'],
-        toChannel: '1441301333',
-        eventType: '138311609000106303',
-        id: 'ABCDEF-12345678901',
-        content: {
-          id: '325708',
-          createdTime: 1332394961610,
-          from: 'uff2aec188e58752ee1fb0f9507c6529a',
-          to: ['u0a556cffd4da0dd89c94fb36e36e1cdc'],
-          toType: 1,
-          contentType: 1,
-          text: 'hello'
-        }
-      },
-      {
-        from: 'u206d25c2ea6bd87c17655609a1c37cb8',
-        fromChannel: '1341301815',
-        to: ['u0cc15697597f61dd8b01cea8b027050e'],
-        toChannel: '1441301333',
-        eventType: '138311609100106403',
-        id: 'ABCDEF-12345678902',
-        content: {
-          revision: 2469,
-          opType: 4,
-          params: [
-            'u0f3bfc598b061eba02183bfc5280886a',
-            null,
-            null
-          ]
-        }
-      }
-    ]
-  };
+  var json = '{\n\
+  "result":[\n\
+    {\n\
+      "from":"u206d25c2ea6bd87c17655609a1c37cb8",\n\
+      "fromChannel":"1341301815",\n\
+      "to":["u0cc15697597f61dd8b01cea8b027050e"],\n\
+      "toChannel":"1441301333",\n\
+      "eventType":"138311609000106303",\n\
+      "id":"ABCDEF-12345678901",\n\
+      "content":{\n\
+        "id":"325708",\n\
+        "createdTime":1332394961610,\n\
+        "from":"uff2aec188e58752ee1fb0f9507c6529a",\n\
+        "to":["u0a556cffd4da0dd89c94fb36e36e1cdc"],\n\
+        "toType":1,\n\
+        "contentType":1,\n\
+        "text":"hello"\n\
+      }\n\
+    },\n\
+    {\n\
+      "from":"u206d25c2ea6bd87c17655609a1c37cb8",\n\
+      "fromChannel":"1341301815",\n\
+      "to":["u0cc15697597f61dd8b01cea8b027050e"],\n\
+      "toChannel":"1441301333",\n\
+      "eventType":"138311609100106403",\n\
+      "id":"ABCDEF-12345678902",\n\
+      "content":{\n\
+        "revision":2469,\n\
+        "opType":4,\n\
+        "params":[\n\
+          "u0f3bfc598b061eba02183bfc5280886a",\n\
+          null,\n\
+          null\n\
+        ]\n\
+      }\n\
+    }\n\
+  ]\n\
+}';
 
   var signature = 'kPXp0nPWSzfWAapWHiesbcztpKnXJoX8krCa1CcTghk=';
 
   it('should be able to validate signature', function(done) {
-    SignatureValidator.validateSignature(JSON.stringify(json), config.channelSecret, signature).should.be.true;
-    SignatureValidator.validateSignature(JSON.stringify(json), config.channelSecret, 'XXX').should.be.false;
-
+    SignatureValidator.validateSignature(json, config.channelSecret, signature).should.be.true;
+    SignatureValidator.validateSignature(json, config.channelSecret, 'XXX').should.be.false;
     done();
   });
 });
