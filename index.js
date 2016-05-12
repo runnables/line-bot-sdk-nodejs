3a2b9d38ce48d811507dc99969cabb7be1b3acb9
Komthep Saksriyuttana <komthep@gmail.com>
2016-05-12 11:46:32 +0700

    rename 'bot.js' to 'client.js'

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
   client: function(config) {
-    return require('./client/bot').init(config);
+    return require('./lib/client').init(config);
   },
   ContentType: require('./lib/constants/ContentType'),
   Markup: require('./lib/message/Markup'),
