2eb3b2dce48c95461318eda7f72c27b911c63916
nsmag <natthakit.susanthitanon@gmail.com>
2016-06-02 15:01:34 +0700

    Export EventType, OpType, RecipientType

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ module.exports = {
     return require('./lib/client').init(config);
   },
   ContentType: require('./lib/constants/ContentType'),
+  EventType: require('./lib/constants/EventType'),
+  OpType: require('./lib/constants/OpType'),
+  RecipientType: require('./lib/constants/RecipientType'),
   Markup: require('./lib/message/Markup'),
   MultipleMessages: require('./lib/message/MultipleMessages')
 };
